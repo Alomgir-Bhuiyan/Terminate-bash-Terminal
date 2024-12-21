26098f5dacf5ee7b717b5c8b06b3e30f85e4ad83
Alomgir-Bhuiyan <alomgirbhuiyan16@gmail.com>
2024-12-21 15:52:10 +0600

    Bug Removed

--- bs.sh
+++ bs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-echo "Your ip address is   : $(hostname -I)"
+echo "Your ip address is   : $(wget -qO- https://ipecho.net/plain)"
 echo "Your architecture is : $(uname -m)"
 echo "You are using        : $(uname -o)"
-printf -v lines "$(date +%Y-%m-%d) at $(date +%H:%M:%S) by $(hostname -I)> User: $(whoami)"
+printf -v lines "$(date +%Y-%m-%d) at $(date +%H:%M:%S) by $(wget -qO- https://ipecho.net/plain ; echo)> User: $(whoami)"
 echo "$lines" >> checked.dmp
 sort -n checked.dmp --reverse --output=checked.dmp
 echo "Checked time         : $(date +%Y-%m-%d) at $(date +%H:%M:%S)"
@@ -11,4 +10,3 @@ echo ""
 echo "=============== System log =================="
 cat checked.dmp
 echo "============================================="
-
--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 echo "Started setting up..."
-path = pwd
-cd && mkdir ../usr/share/tb/
-mv $path/* ../usr/share/tb/
-chmod +x ../usr/share/tb/*
-soco "bash ../usr/share/tb/bs.sh"
-echo "$soco" >> ../usr/bin/tb
-chmod +x ../usr/bin/tb
+mkdir /data/data/com.termux/files/usr/share/tb
+cp * /data/data/com.termux/files/usr/share/tb
+chmod +x /data/data/com.termux/files/usr/share/tb/*
+echo "bash /data/data/com.termux/files/usr/share/tb/bs.sh" >> /data/data/com.termux/files/usr/bin/tb
+chmod +x /data/data/com.termux/files/usr/bin/tb
 echo "finished !"
