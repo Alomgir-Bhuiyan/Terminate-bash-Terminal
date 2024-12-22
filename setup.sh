b5d7aa9e95ca38e8b99308cd92dba6a36512317d
Alomgir-Bhuiyan <alomgirbhuiyan16@gmail.com>
2024-12-22 11:24:41 +0600

    Improved

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 echo "Started setting up..."
 mkdir /data/data/com.termux/files/usr/share/tb
-cp * /data/data/com.termux/files/usr/share/tb
+mv * /data/data/com.termux/files/usr/share/tb
 chmod +x /data/data/com.termux/files/usr/share/tb/*
 echo "bash /data/data/com.termux/files/usr/share/tb/bs.sh" >> /data/data/com.termux/files/usr/bin/tb
 chmod +x /data/data/com.termux/files/usr/bin/tb
+cd .. && rm -rf Terminate-bash-Terminal
 echo "finished !"
